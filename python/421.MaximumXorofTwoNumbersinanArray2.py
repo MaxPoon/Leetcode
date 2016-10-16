3a2a06d3b644a29905376e425fd7d83452e4736f
maxpoon <maxpanziyuan@gmail.com>
2016-10-16 18:34:47 +0800

    upload solution

--- python/421.MaximumXorofTwoNumbersinanArray2.py
+++ python/421.MaximumXorofTwoNumbersinanArray2.py
@@ -8,7 +8,6 @@ class Solution(object):
         for x in range(31, -1, -1):
             mask += 1 << x
             prefixSet = set([n & mask for n in nums])
-            print(prefixSet)
             tmp = ans | 1 << x
             for prefix in prefixSet:
                 if tmp ^ prefix in prefixSet:
