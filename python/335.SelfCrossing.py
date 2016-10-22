ed55d4041eb3129dab002c74c5c970c974f40971
maxpoon <maxpanziyuan@gmail.com>
2016-10-22 20:34:28 +0800

    upload solutions

---
+++ python/335.SelfCrossing.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isSelfCrossing(self, x):
+        """
+        :type x: List[int]
+        :rtype: bool
+        """
+        if len(x)<4:
+            return False
+        for i in range(3,len(x)):
+            if (x[i]>=x[i-2] and x[i-1]<=x[i-3]): return True
+            if (i>=4 and x[i-1]==x[i-3] and x[i]+x[i-4]>=x[i-2]): return True
+            if (i>=5 and x[i-1]<=x[i-3] and x[i-5]+x[i-1]>=x[i-3] and x[i]+x[i-4]>=x[i-2] and x[i-2]>=x[i-4]):  return True
+        return False
