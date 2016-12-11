7b5e8b035fc0cb2c7da769f17ed03e7b53cf8b13
Max Poon <maxpanziyuan@gmail.com>
2016-12-11 22:03:06 +0800

    modify solution

--- python/198.HouseRobber.py
+++ python/198.HouseRobber.py
@@ -1,20 +1,20 @@
 class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
-        if n==0:
-            return 0
-        if n==1:
-            return nums[0]
-        if n==2:
-            return max(nums)
-        dp = [0]*n
-        dp[0]=nums[0]
-        dp[1]=nums[1]
-        dp[2]=nums[2] + nums[0]
-        for i in range(3,n):
-            dp[i]=max(dp[i-2],dp[i-3])+nums[i]
-        return max(dp[-1], dp[-2])
+	def rob(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		n = len(nums)
+		if n==0:
+			return 0
+		if n==1:
+			return nums[0]
+		if n==2:
+			return max(nums)
+		dp = [0]*n
+		dp[0]=nums[0]
+		dp[1]=nums[1]
+		dp[2]=nums[2] + nums[0]
+		for i in range(3,n):
+			dp[i]=max(dp[i-2],dp[i-3])+nums[i]
+		return max(dp[-1], dp[-2])
