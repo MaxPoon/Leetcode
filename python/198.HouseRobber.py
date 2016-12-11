30c128687349f965a0cae5e2ffad822e97130964
Max Poon <maxpanziyuan@gmail.com>
2016-12-11 22:02:50 +0800

    modify solution

--- python/198.HouseRobber.py
+++ python/198.HouseRobber.py
@@ -11,11 +11,10 @@ class Solution(object):
             return nums[0]
         if n==2:
             return max(nums)
-        else:
-            dp = [0]*n
-            dp[0]=nums[0]
-            dp[1]=nums[1]
-            dp[2]=nums[2] + nums[0]
-            for i in range(3,n):
-                dp[i]=max(dp[i-2],dp[i-3])+nums[i]
-            return max(dp[-1], dp[-2])
+        dp = [0]*n
+        dp[0]=nums[0]
+        dp[1]=nums[1]
+        dp[2]=nums[2] + nums[0]
+        for i in range(3,n):
+            dp[i]=max(dp[i-2],dp[i-3])+nums[i]
+        return max(dp[-1], dp[-2])
