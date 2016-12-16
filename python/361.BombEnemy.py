37cb6c2a1349ac462d337cceefb0a2017218c905
Max Poon <maxpanziyuan@gmail.com>
2016-12-16 16:43:26 +0800

    update solution, better version

--- python/361.BombEnemy.py
+++ python/361.BombEnemy.py
@@ -4,41 +4,37 @@ class Solution(object):
 		:type grid: List[List[str]]
 		:rtype: int
 		"""
-		if grid is None or len(grid)==0 or len(grid[0]) == 0: return 0
-		rows = len(grid)
-		cols = len(grid[0])
-		left = [[0]*cols for _ in range(rows)]
-		right = [[0]*cols for _ in range(rows)]
-		up = [[0]*cols for _ in range(rows)]
-		down = [[0]*cols for _ in range(rows)]
-		currentMax = 0
-		for row in range(rows):
-			current = 0
-			for col in range(cols):
-				if grid[row][col] == 'W': current = 0
-				left[row][col] = current
-				if grid[row][col] == 'E': current+=1
-		for row in range(rows):
-			current = 0
-			for col in range(cols-1, -1, -1):
-				if grid[row][col] == 'W': current = 0
-				right[row][col] = current
-				if grid[row][col] == 'E': current+=1
-		for col in range(cols):
-			current = 0
-			for row in range(rows):
-				if grid[row][col] == 'W': current = 0
-				up[row][col] = current
-				if grid[row][col] == 'E': current+=1
-		for col in range(cols):
-			current = 0
-			for row in range(rows-1, -1, -1):
-				if grid[row][col] == 'W': current = 0
-				down[row][col] = current
-				if grid[row][col] == 'E': current+=1
-		for row in range(rows):
-			for col in range(cols):
-				if grid[row][col]=='W' or grid[row][col] == 'E': continue
-				temp = left[row][col] + right[row][col] + up[row][col] + down[row][col] 
-				currentMax = max(currentMax, temp)
-		return currentMax
+class Solution(object):
+	def maxKilledEnemies(self, grid):
+		"""
+		:type grid: List[List[str]]
+		:rtype: int
+		"""
+		m, n = len(grid), 0
+		if m:
+			n = len(grid[0])
+		result, rows = 0, 0
+		cols = [0 for i in range(n)]
+
+		for i in range(m):
+			for j in range(n):
+				if j == 0 or grid[i][j-1] == 'W':
+					rows = 0
+					for k in range(j, n):
+						if grid[i][k] == 'W':
+							break
+						if grid[i][k] == 'E':
+							rows += 1
+
+				if i == 0 or grid[i-1][j] == 'W':
+					cols[j] = 0
+					for k in range(i, m):
+						if grid[k][j] == 'W':
+							break
+						if grid[k][j] == 'E':
+							cols[j] += 1
+
+				if grid[i][j] == '0' and rows + cols[j] > result:
+					result = rows + cols[j]
+
+		return result
