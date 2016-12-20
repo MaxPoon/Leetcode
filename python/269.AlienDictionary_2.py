394984f6037a249b11885de02e81b526551c4633
Max Poon <maxpanziyuan@gmail.com>
2016-12-20 15:41:10 +0800

    add another solution, slow

---
+++ python/269.AlienDictionary_2.py
@@ -0,0 +1,71 @@
+class Solution(object):
+	def alienOrder(self, words):
+		"""
+		:type words: List[str]
+		:rtype: str
+		"""
+		if len(words) == 0 : return ""
+		if len(words)==1: return words[0]
+		prev = {}
+		next = {}
+		charSet =set()
+		added = ""
+		for i in range(len(words)-1):
+			word1 = words[i]
+			word2 = words[i+1]
+			p1 = 0
+			p2 = 0
+			while p1<len(word1) and p2<len(word2):
+				charSet.add(word1[p1])
+				charSet.add(word2[p2])
+				if word1[p1] != word2[p2]: break
+				p1+=1
+				p2+=1
+			if p2==len(word2) and p1<len(word1): return ""
+			if p1==len(word1):
+				while p2<len(word2):
+					charSet.add(word2[p2])
+					p2+=1
+				continue
+			if p2==len(word2):
+				while p1<len(word1):
+					charSet.add(word1[p1])
+					p1+=1
+				continue            
+			c1 = word1[p1]
+			c2 = word2[p2]
+			while p1<len(word1):
+				charSet.add(word1[p1])
+				p1+=1
+			while p2<len(word2):
+				charSet.add(word2[p2])
+				p2+=1
+			if c1 in next:
+				next[c1].append(c2)
+			else:
+				next[c1] = [c2]
+			if c2 in prev:
+				prev[c2].add(c1)
+			else:
+				prev[c2] = set(c1)
+		l = len(charSet)
+		zeroDegree = []
+		for c in charSet:
+			if c not in prev or len(prev[c])==0:
+				zeroDegree.append(c)
+		for c in zeroDegree:
+			charSet.remove(c)
+		i = 0
+		while i<len(zeroDegree):
+			c = zeroDegree[i]
+			i+=1
+			if c in prev and len(prev[c]) != 0: return ""
+			added += c
+			if c not in next: continue
+			for c2 in next[c]:
+				if c2 in prev and c in prev[c2]:
+					prev[c2].remove(c)
+					if len(prev[c2]) == 0:
+						zeroDegree.append(c2)
+		if len(added)<l: return ""  
+		return added
