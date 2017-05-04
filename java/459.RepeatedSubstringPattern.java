ee128ca8a020ec551950aeb3d2dd6564dab90a62
Pan <zipan@paypal.com>
2017-05-04 15:53:21 +0800

    add solution

---
+++ java/459.RepeatedSubstringPattern.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+        int[] prefix = kmp(str);
+        int len = prefix[str.length()-1];
+        int n = str.length();
+        return (len > 0 && n%(n-len) == 0);
+    }
+    private int[] kmp(String s){
+        int len = s.length();
+        int[] res = new int[len];
+        char[] ch = s.toCharArray();
+        int i = 0, j = 1;
+        res[0] = 0;
+        while(i < ch.length && j < ch.length){
+            if(ch[j] == ch[i]){
+                res[j] = i+1;
+                i++;
+                j++;
+            }else{
+                if(i == 0){
+                    res[j] = 0;
+                    j++;
+                }else{
+                    i = res[i-1];
+                }
+            }
+        }
+        return res;
+    }
+}
