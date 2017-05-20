fee7560ae8fec119b7f7287a99253148fd5e14c7
Pan <zipan@paypal.com>
2017-05-21 00:50:13 +0800

    refactor the code

--- java/53.MaximumSubarray.java
+++ java/53.MaximumSubarray.java
@@ -1,15 +1,12 @@
 public class Solution {
-	public int maxSubArray(int[] nums) {
-		int dp[] = new int[nums.length];
-		dp[0] = nums[0];
-		for(int i=1; i<nums.length; i++){
-			if(dp[i-1]<0) dp[i] = nums[i];
-			else dp[i] = nums[i] + dp[i-1];
-		}
-		int max = dp[0];
-		for(int i=1; i<nums.length; i++){
-			max = Math.max(max, dp[i]);
-		}
-		return max;
-	}
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i=1; i<nums.length; i++) {
+            dp[i] = dp[i-1] > 0 ? dp[i-1] + nums[i] : nums[i];
+            max = dp[i] > max ? dp[i] : max;
+        }
+        return max;
+    }
 }
